e507750becdb7a072e8c4b6d880a5bc99ade8e7c
LinhNguyen1505 <danhauloc@gmail.com>
2022-09-22 16:39:47 +0700

    a

--- src/component/album.js
+++ src/component/album.js
@@ -1,45 +1,46 @@
-import React from "react"
-import axios from "axios"
-import { useState } from "react"
-import { Link } from 'react-router-dom'
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const GetImgs = new Promise((resolve, reject) => {
-    axios.get('https://halee-portfolio.herokuapp.com/loadImg')
-        .then((res) => resolve(res.data))
-}
-)
+  axios
+    .get("https://halee-portfolio.herokuapp.com/loadImg")
+    .then((res) => resolve(res.data));
+});
 
 function ShowAlbum() {
-    const [albums, setAlbums] = useState(null)
-    GetImgs.then(
-        (res) => setAlbums(res)
-    )
-    console.log(albums)
-    if (albums) {
-        return (
-            <div className="main">
-                <div className="intro">
-                    <div className="albums-description">
-                        {albums.map((album, index) =>
-                            <Link key={index} className="album" to={`/album/${album.gallery}`}>
-                                {/* href */}
-                                <div className="album-thumbnail" style={{ backgroundImage: "url(" + album.path[0] + ")" }}>
-                                    <div className="album-overlay">
-                                        <h3 className="album-name">{album.gallery}</h3>
-                                    </div>
-                                </div>
-                            </Link>
-                        )}
-                    </div>
-
+  const [albums, setAlbums] = useState(null);
+  GetImgs.then((res) => setAlbums(res));
+  console.log(albums);
+  if (albums) {
+    return (
+      <div className="main">
+        <div className="intro">
+          <div className="albums-description">
+            {albums.map((album, index) => (
+              <Link
+                key={index}
+                className="album"
+                to={`/album/${album.gallery}`}
+              >
+                {/* href */}
+                <div
+                  className="album-thumbnail"
+                  style={{ backgroundImage: "url(" + album.path[0] + ")" }}
+                >
+                  <div className="album-overlay">
+                    <h3 className="album-name">{album.gallery}</h3>
+                  </div>
                 </div>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div></div>
-        )
-    }
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 }
-export default ShowAlbum
+export default ShowAlbum;
